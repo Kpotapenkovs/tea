4bc92263c7d2159f7775d4f61a6bdb19caf6f89f
daniels <danmel1234567@gmail.com>
2026-02-23 14:28:13 +0200

    edit design

--- resources/views/tea/edit.blade.php
+++ resources/views/tea/edit.blade.php
@@ -7,24 +7,21 @@
         @method("PUT")
 
         Tējas nosaukums
-        <input type="text" id="tea_name" name="tea_name" value="{{ $tea->tea_name }}" required>
-        <br>
+        <input class="edit_input_field" type="text" id="tea_name" name="tea_name" value="{{ $tea->tea_name }}" required>
         Bonusa uzkoda
-        <input type="text" id="bonus_snack" name="bonus_snack" value="{{ $tea->bonus_snack }}" >
-        <br>
+        <input class="edit_input_field" type="text" id="bonus_snack" name="bonus_snack" value="{{ $tea->bonus_snack }}" >
         Cukura daudzums
-        <input type="number" id="shugar" name="shugar" value="{{ $tea->shugar }}">
-        <br>
+        <input class="edit_input_field" type="number" id="shugar" name="shugar" value="{{ $tea->shugar }}">
         Plānošanas laiks
-        <input type="datetime-local" id="planing_time" name="planing_time" value="{{ $tea->planing_time }}" required>
-        <br>
-        favorite
+        <input class="edit_input_field" type="datetime-local" id="planing_time" name="planing_time" value="{{ $tea->planing_time }}" required>
+        make favorite
         <label class="favorite-checkbox">
         <input name="favorite" type="hidden" value="0">
         <input name="favorite" type="checkbox" value="1" {{ old("favorite", $tea->favorite) ? 'checked' : '' }}>
         <span class="checkmark"></span>
         </label>
         <br>  
+        <br>
         <button type="submit" class="submit_button">Saglabāt izmaiņas</button>
     </form>
 </div>
